872de032f1f3ee0bfa0f1f01fe8324414015be72
nmessa <nmessa@sau16.org>
2020-03-13 08:26:37 -0400

    Add files via upload

---
+++ Lab Exercise 3.12.2020/Workshop Registration/Workshop Registration/Form1.cs
@@ -0,0 +1,67 @@
+﻿//Lab Exercise 3.12.2020 Problem 2
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Workshop_Registration
+{
+    public partial class Form1 : Form
+    {
+        int total;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //Declare variables
+            int days=0, regFee=0, lodging=0;
+
+            //get workshop registration fee and days
+            //Add code here
+
+
+
+
+            //calculate lodging cost
+            //Add code here
+
+
+            //Calculate to cost
+            //Add code here
+
+
+            //Add total amount to lstCosts as currency
+            //Add code here
+
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            //put total in lblCost as currency
+            //Add code here
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+    }
+}
---
+++ Lab Exercise 3.13.2020/Blocks/Blocks/Form1.cs
@@ -0,0 +1,131 @@
+﻿//Lab Exercise 3.13.2020
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Blocks
+{
+    public partial class Form1 : Form
+    {
+        //Global variables
+        //Add code here
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        //Define Property BallX
+        public int BallX
+        {
+            //Add code here
+
+        }
+
+        //Define Property BallY
+        public int BallY
+        {
+            //Add code here
+
+        }
+
+        //Define Property Lives
+        public int Lives
+        {
+            //Add code here
+
+        }
+
+        //Define Property SpeedX
+        public int SpeedX
+        {
+            //Add code here
+
+        }
+
+        //Define Property SpeedY
+        public int SpeedY
+        {
+            //Add code here
+
+        }
+
+        //Define Property Score
+        public int Score
+        {
+            //Add code here
+
+        }
+
+        //Define Property AllGone
+        public int AllGone
+        {
+            //Add code here
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Add code here
+
+
+
+        }
+
+        public void CheckCollisions()
+        {
+            //Add code here
+
+
+
+        }
+        
+        public void CheckCollision(PictureBox src , Boolean Hide)
+        {
+            //Add code here
+
+
+        }
+
+        public void CheckCollision(PictureBox src)
+        {
+            //Add code here
+
+
+        }
+
+        public void UpdateScore()
+        {
+            //Add code here
+
+
+        }
+
+        public void UpdateLives()
+        {
+            //Add code here
+
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Add code here
+
+        } 
+
+    }
+}
