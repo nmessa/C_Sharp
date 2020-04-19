af7260d1ec8df63052ee7efaa6e437b54a24c051
nmessa <nmessa@sau16.org>
2020-04-19 07:39:27 -0400

    Add files via upload

---
+++ Lab Exercise 4.20.2020/Dorm and Meal Cost/Dorm and Meal Cost/frmDorm.cs
@@ -0,0 +1,31 @@
+﻿//Lab Exercise 4.20.2020
+//Author: nmessa
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Dorm_and_Meal_Cost
+{
+    public partial class frmDorm : Form
+    {
+        public frmDorm()
+        {
+            InitializeComponent();
+        }
+
+        //This function will return dorm information to frmMain
+        //dormCost and myDorm
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+
+        }
+    }
+}
